a03dacfdfd986db4487e9b2e19db0ae0ee2c8b6d
toomanysulkies <toomanysulkies@gmail.com>
2021-05-05 23:19:10 +0900

    0505

--- js/01.video.js
+++ js/01.video.js
@@ -1,17 +1,29 @@
 var options = {
 	videoURL: 'MxvQCa2cEGU', //동영상의 소유자가 엠베드를 허용하지 않은 경우 재생이 안될 수 있다. 콘솔 창에 ypotube nocookie뜸!!! 
-	containment: '.video-wrapper',
+	containment: '.video-wrapper2',
 	autoPlay: true,
 	mute: true,
 	startAt: 0,
 	opacity: 1,
 	loop: true,
-	ratio: '4/3',
+	ratio: 'auto',
 	addRaster: false,
+	showControls: false,
+	showYTLogo: false,
 
 
 }
 $('#youtubeBg').YTPlayer(options);
+
+var $video = $('.video-wrapper video')
+$('window').resize(onResize)
+function onResize() {
+	var windowWidth = $(this).innerWidth()
+	var videoWidth = $video.innerWidth();
+	if (windowWidth < videoWidth) {
+		$video.css.removeClass
+	}
+}
 
 
 
