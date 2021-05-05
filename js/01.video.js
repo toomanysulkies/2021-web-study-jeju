1fad8cef9423422c99c6ca67bac99de4ca644cfe
toomanysulkies <toomanysulkies@gmail.com>
2021-05-05 21:20:18 +0900

    0504_add ytplayer

---
+++ js/01.video.js
@@ -0,0 +1,16 @@
+var options = {
+	videoURL: 'uM9aurfeh6U',
+	containment: '.video-wrapper',
+	autoplay: true,
+	mute: true,
+	startAt: 0,
+	opacity: 1,
+	loop: true,
+	ratio: "4/3",
+	addRaster: true
+}
+
+
+
+
+$('#youtubeBg').YTPlayer(options);
