7e1b11ebc616f46a09a80fcdd0d64e63aed03ceb
toomanysulkies <toomanysulkies@gmail.com>
2021-05-06 01:59:17 +0900

    0505

--- js/01.video.js
+++ js/01.video.js
@@ -7,11 +7,15 @@ var options = {
 	opacity: 1,
 	loop: true,
 	ratio: 'auto',
-	addRaster: false,
+addRaster: false,
 	showControls: false,
 	showYTLogo: false,
-
-
+	onReady: function() {
+		
+	},
+	onError: function() {
+		alert('동영상을 가져오는데 에러가 발생하였습니다.')
+	}
 }
 $('#youtubeBg').YTPlayer(options);
 
@@ -20,14 +24,14 @@ $('window').resize(onResize)
 function onResize() {
 	var windowWidth = $(this).innerWidth()
 	var videoWidth = $video.innerWidth();
-	if (windowWidth < videoWidth) {
-		$video.css({ 'width': '100%', 'height': 'auto' })
-		//즉 윈도우의 크기가 동영상의 크기보다 작으면 너비값을 100%로 맞춰라
+	// if (windowWidth < videoWidth) {
+	// 	$video.css({ 'width': '100%', 'height': 'auto' })
+	// 	//즉 윈도우의 크기가 동영상의 크기보다 작으면 너비값을 100%로 맞춰라
 
-	}
-	else {
-		$video.css({ 'height': '100%', 'width': 'auto' })
-	}
+	// }
+	// else {
+	// 	$video.css({ 'height': '100%', 'width': 'auto' })
+	// }
 }
 
 
