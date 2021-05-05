ec54abcd2f1580517355a90b3b89a952dee2b0f4
toomanysulkies <toomanysulkies@gmail.com>
2021-05-05 22:00:01 +0900

    0505_solve the problem youtube no cookie!

--- js/01.video.js
+++ js/01.video.js
@@ -1,16 +1,20 @@
 var options = {
-	videoURL: 'uM9aurfeh6U',
+	videoURL: 'MxvQCa2cEGU', //동영상의 소유자가 엠베드를 허용하지 않은 경우 재생이 안될 수 있다. 콘솔 창에 ypotube nocookie뜸!!! 
 	containment: '.video-wrapper',
-	autoplay: true,
+	autoPlay: true,
 	mute: true,
 	startAt: 0,
 	opacity: 1,
 	loop: true,
-	ratio: "4/3",
-	addRaster: true
+	ratio: '4/3',
+	addRaster: false,
+
+
 }
-
-
-
-
 $('#youtubeBg').YTPlayer(options);
+
+
+
+// jQuery(function () {
+// 	jQuery("#[playerID]").YTPlayer();
+// });
