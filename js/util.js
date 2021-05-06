1ea760737d3f739143eb9a379206d5ede7c2d00b
toomanysulkies <toomanysulkies@gmail.com>
2021-05-06 17:28:45 +0900

    0506 scroll spy2

--- js/util.js
+++ js/util.js
@@ -1,3 +1,33 @@
+
+
+
+
+/*************** Scroll Spy *****************/
+function spyScroll(el, cls, _gap) {
+	$(window).scroll(onSpyScroll).trigger('scroll')
+	function onSpyScroll() {
+		var scrollTop = $(this).scrollTop()
+		var pageOffset = []
+		var page
+		var gap = _gap || 300
+		$(el).each(function(i){
+			pageOffset[i] = $(this).offset().top
+		})
+
+		for(var i=1; i<pageOffset.length; i++) {
+			if(scrollTop < pageOffset[i] - gap) break
+		}
+		page = i - 1
+		$(el).eq(page).addClass(cls)
+	}
+}
+
+
+
+/***************************************** Array.sort() ********************************************* */
+
+
+
 function sortAsc(key) {
 	return function (a, b) {
 		return key ? a[key] - b[key] : a - b
