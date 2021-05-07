0d5c3965a0e6bbe677e14d8e117224cf1faef414
toomanysulkies <toomanysulkies@gmail.com>
2021-05-08 04:06:26 +0900

    0507_navigation

---
+++ js/common.js
@@ -0,0 +1,60 @@
+/*************** 글로벌 설정 *****************/
+initCommon()
+
+/*************** 사용자 함수 *****************/
+function initCommon() {
+	$('.header-wrapper').find('.notice-content').hide()
+	$('.header-wrapper').find('.bt-hide').hide()
+	if ($.cookie('hideNotice') === 'Y') onCloseNotice()
+	else {
+		$('.header-wrapper').find('.notice-content').hide()
+		$('.header-wrapper').find('.bt-hide').hide()
+	}
+}
+/*******************/
+
+
+/*************** 이벤트 등록 *****************/
+$('.header-wrapper .bt-show').click(onShowNotice)
+$('.header-wrapper .bt-hide').click(onHideNotice)
+$('.header-wrapper .bt-close').click(onCloseNotice)
+$('.header-wrapper .bt-today').click(onHideTodayNotice)
+$('.header-wrapper .link-lang').click(onToggleLang)
+$('.header-wrapper .link-lang').mouseleave(onHideLang)
+$('.header-wrapper .link-lang .lang').click(onChangeLang)
+/*************** 이벤트 콜백 *****************/
+function onShowNotice() {
+	$('.header-wrapper').find('.bt-show').hide()
+	$('.header-wrapper').find('.bt-hide').show()
+	$('.header-wrapper').find('.notice-content').show()
+}
+function onHideNotice() {
+	$('.header-wrapper').find('.bt-hide').hide()
+	$('.header-wrapper').find('.bt-show').show()
+	$('.header-wrapper').find('.notice-content').hide()
+
+}
+function onCloseNotice() {
+	$('.header-wrapper').find('.notice-wrapper').hide()
+
+}
+function onHideTodayNotice() {
+	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
+	onCloseNotice
+}
+
+function onToggleLang() {
+$('.header-wrapper .link-lang .hover-lang').toggle()
+}
+function onHideLang() {
+	$('.header-wrapper .link-lang .hover-lang').hide()
+}
+
+function onChangeLang() {
+  var $span = $(this).parent().prev().find('span')
+	var myLang = $(this).text()
+	var spanLang = $span.text()
+	$span.text(myLang)
+ $(this).text(spanLang)
+
+}
