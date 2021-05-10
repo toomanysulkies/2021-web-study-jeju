958d4abc2474f6e454fb0d09c839781b75121e24
toomanysulkies <toomanysulkies@gmail.com>
2021-05-10 20:27:07 +0900

    0510

--- js/common.js
+++ js/common.js
@@ -1,10 +1,11 @@
 /*************** 글로벌 설정 *****************/
+var isHeaderAni = false
 initCommon()
+
 
 /*************** 사용자 함수 *****************/
 function initCommon() {
-
-	if ($.cookie('hideNotice') === 'Y') onCloseNotice()
+	if($.cookie('hideNotice') === 'Y') onCloseNotice()
 	else {
 		$('.notice-wrapper').find('.notice-content').hide()
 		$('.notice-wrapper').find('.bt-hide').hide()
@@ -12,79 +13,104 @@ function initCommon() {
 }
 
 function scrollNotice(scTop) {
-	var noticeHeight = $('.notice-wrapper').outerHeight()
-	var linkHeight = $('.link-wrapper').outerHeight()
-		if (scTop > 0){
-		$('.notice-wrapper').hide()
-	$('.header-wrapper').css('top', scTop-linkHeight + 'px')
-   }
-	else {
-	$('.notice-wrapper').show()
-	$('.header-wrapper').css('top', 'unset')
+	var $notice = $('.notice-wrapper')
+	var $link = $('.link-wrapper')
+	var $header = $('.header-wrapper')
+	var headerHeight
+	if(scTop == 0) {
+		$notice.show()
+		$link.show()
+		$header.css('top', 'unset')
+		$header.removeClass('active')
 	}
-}
-function scrollHeader(scTop) {
-		var headerGap = $('.header-wrapper').outerHeight()
-		if (headerGap < scTop) {
-		$('.header-wrapper').css({
-			'position': 'fixed', 'top': 0})
-	}
+	/* else if(scTop < 150) {
+		$notice.hide()
+		$link.hide()
+		$header.css('top', 'unset')
+		$header.removeClass('active')
+	}*/
 	else {
-		$('.header-wrapper').css({
-			'position': 'absolute', 'top': 'unset'})
+		$notice.hide()
+		$link.hide()
+		headerHeight = $header.outerHeight()
+		$header.css('top', -headerHeight + 'px')
+		$header.css('top')
+		$header.css('top', 0)
+		$header.addClass('active')
 	}
 }
 
+/* function scrollNotice(scTop) {
+	var $notice = $('.notice-wrapper')
+	var $link = $('.link-wrapper')
+	if(scTop == 0) {
+		$notice.show()
+		$link.show()
+	}
+	else {
+		$notice.hide()
+		$link.hide()
+	}
+} */
+
+
 /*************** 이벤트 등록 *****************/
 $(window).scroll(onScroll).trigger('scroll')
-	
+
 $('.notice-wrapper .bt-show').click(onShowNotice)
 $('.notice-wrapper .bt-hide').click(onHideNotice)
 $('.notice-wrapper .bt-close').click(onCloseNotice)
 $('.notice-wrapper .bt-today').click(onHideTodayNotice)
+
 $('.header-wrapper .link-lang').click(onToggleLang)
+$('.header-wrapper .link-lang').mouseenter(onShowLang)
 $('.header-wrapper .link-lang').mouseleave(onHideLang)
-$('.header-wrapper .link-lang .lang').click(onChangeLang)
+$('.header-wrapper .link-lang .lang').click(onChgLang)
+
+
 /*************** 이벤트 콜백 *****************/
 function onScroll(e) {
 	var scTop = $(this).scrollTop()
-  scrollNotice(scTop)
-  scrollHeader(scTop)
-  // scrollNavi(scTop)
+	scrollNotice(scTop)
 }
-   
+
 function onShowNotice() {
 	$('.notice-wrapper').find('.bt-show').hide()
 	$('.notice-wrapper').find('.bt-hide').show()
 	$('.notice-wrapper').find('.notice-content').show()
 }
-function onHideNotice() {
-	$('.notice-wrapper').find('.bt-hide').hide()
-	$('.notice-wrapper').find('.bt-show').show()
-	$('.notice-wrapper').find('.notice-content').hide()
 
+function onHideNotice() {
+	$('.notice-wrapper').find('.bt-show').show()
+	$('.notice-wrapper').find('.bt-hide').hide()
+	$('.notice-wrapper').find('.notice-content').hide()
 }
+
 function onCloseNotice() {
 	$('.notice-wrapper').hide()
-
 }
+
 function onHideTodayNotice() {
 	$.cookie('hideNotice', 'Y', { expires: 1, path: '/' })
-	onCloseNotice
+	onCloseNotice()
 }
 
 function onToggleLang() {
-$('.header-wrapper .link-lang .hover-lang').toggle()
+	$('.header-wrapper .link-lang .hover-lang').toggle()
 }
+
+function onShowLang() {
+	$('.header-wrapper .link-lang .hover-lang').show()
+}
+
 function onHideLang() {
 	$('.header-wrapper .link-lang .hover-lang').hide()
 }
 
-function onChangeLang() {
-  var $span = $(this).parent().prev().find('span')
+function onChgLang() {
+	var $span = $(this).parent().prev().find('span')
 	var myLang = $(this).text()
 	var spanLang = $span.text()
 	$span.text(myLang)
- $(this).text(spanLang)
-
+	$(this).text(spanLang)
 }
