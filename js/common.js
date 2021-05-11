d899ef2d010e7264479299a9b8d665e534c5274f
toomanysulkies <toomanysulkies@gmail.com>
2021-05-12 00:10:31 +0900

    ...

--- js/common.js
+++ js/common.js
@@ -11,29 +11,49 @@ function initCommon() {
 	}
 }
 
+
 function scrollNotice(scTop) {
-	var noticeHeight = $('.notice-wrapper').outerHeight()
-	var linkHeight = $('.link-wrapper').outerHeight()
-		if (scTop > 0){
-		$('.notice-wrapper').hide()
-	$('.header-wrapper').css('top', scTop-linkHeight + 'px')
-   }
+	var $notice = $('.notice-wrapper')
+	var $link = $('.link-wrapper')
+	var $navi = $('.navi-wrapper')
+	var $header = $('.header-wrapper')
+	var noticeHeight , linkHeight , naviHeight,headerHeight
+	 //notice-wrapper의 바깥높이는  noticeHeight	
+	if (scTop == 0) {
+		$notice.show()//notice-wrapper는 보이고
+		$link.show()//'.link-wrappert도 보인다.
+		$header.css('top', 'unset')
+	}
+	else if ( scTop < 150 ) { //스크롤이 내려가면
+		$notice.hide() //notice-wrapper는 숨겨지고
+		$link.hide()	//.link-wrapper도 숨겨진다
+		
+	}
 	else {
-	$('.notice-wrapper').show()
-	$('.header-wrapper').css('top', 'unset')
+		$notice.hide()
+		$link.hide()
+		headerHeight = $header.outerHeight()
+		$header.css({ 'top':-headerHeight + 'px'})
+
 	}
 }
+
 function scrollHeader(scTop) {
-		var headerGap = $('.header-wrapper').outerHeight()
-		if (headerGap < scTop) {
-		$('.header-wrapper').css({
-			'position': 'fixed', 'top': 0})
+   	var linkHeight = $('.link-wrapper').outerHeight()
+		var headerHeight = $('.header-wrapper').outerHeight()
+	
+	if (headerHeight < scTop) {//window의 scroll 위치가 header-wrapper의 높이값밑으로 내려가면
+
+		$('.header-wrapper').stop().animate({ 'top': 0 }, 200, function(){$('.header-wrapper').css({ 'position': 'fixed', 'top':0}) })//header-wrapper의 position은 높이 0에 고정된다.
 	}
-	else {
-		$('.header-wrapper').css({
-			'position': 'absolute', 'top': 'unset'})
-	}
+	else {//100미만으로 내려가면
+		$('.header-wrapper').css({ 'position': 'absolute', 'top': 'unset' })//position은 부모인 body에 고정되고 top은 초깃값을 유지한다(부모로부터 상속값이 없으면 초깃값유지)
 }
+}
+
+
+
+
 
 /*************** 이벤트 등록 *****************/
 $(window).scroll(onScroll).trigger('scroll')
@@ -46,13 +66,16 @@ $('.header-wrapper .link-lang').click(onToggleLang)
 $('.header-wrapper .link-lang').mouseleave(onHideLang)
 $('.header-wrapper .link-lang .lang').click(onChangeLang)
 /*************** 이벤트 콜백 *****************/
-function onScroll(e) {
-	var scTop = $(this).scrollTop()
+function onScroll(e)
+{
+	var scTop = $(this).scrollTop() //여기서 this는 window, window의 scroll 위치는 변수 scTop
   scrollNotice(scTop)
-  scrollHeader(scTop)
-  // scrollNavi(scTop)
+	scrollHeader(scTop)
+
 }
-   
+
+
+
 function onShowNotice() {
 	$('.notice-wrapper').find('.bt-show').hide()
 	$('.notice-wrapper').find('.bt-hide').show()
